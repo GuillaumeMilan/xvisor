5bead5e83884cb8aaf3a03437593d650b5f9a876
romzie <romain.pierson@ensimag.grenoble-inp.fr>
2017-10-17 14:18:42 +0200

    Added install script for Debian

---
+++ install.sh
@@ -0,0 +1,65 @@
+#! /bin/bash
+
+#Goto directory xvisor-tree
+xvisor_src=$(pwd)
+cd ..
+
+sudo apt-get install flex
+sudo apt-get install bison
+sudo apt-get install genext2fs
+sudo apt-get install telnet
+
+#Install arm-eabi-gcc compiler
+cd tools
+wget http://dn.odroid.in/ODROID-XU/compiler/arm-eabi-4.6.tar.gz
+tar xvf arm-eabi-4.6.tar.gz
+
+export PATH="$PATH":$xvisor_src/../tools/arm-eabi-4.6/bin
+export CROSS_COMPILE=arm-eabi-
+
+arm-eabi-gcc --version
+tmp=echo $?
+if [ tmp -ne 0 ]; then
+    echo "Error installing arm-eabi-gcc"
+    exit 1
+fi
+
+#Update xvisor source
+cd $xvisor_src
+git submodule init
+git submodule update
+
+#Compile for ARM v7
+make ARCH=arm generic-v7-defconfig
+make all dtbs
+
+#Setup FreeRTOS guest
+cd $xvisor_src/..
+wget -O FreeRTOSv9.0.0.zip https://downloads.sourceforge.net/project/freertos/FreeRTOS/V9.0.0/FreeRTOSv9.0.0.zip?r=http%3A%2F%2Fwww.freertos.org%2Fa00104.html&ts=1508233726&use_mirror=vorboss
+unzip FreeRTOSv9.0.0.zip
+
+#Update FreeRTOS files of xvisor
+cp -r ./FreeRTOSv9.0.0/FreeRTOS $xvisor_src/tests/arm32/vexpress-a9/freertos
+cd $xvisor_src/tests/arm32/vexpress-a9/freertos
+mkdir -p port
+cp FreeRTOS/Source/portable/GCC/ARM_CA9/* port/
+patch -p0 < patches/ports.patch
+cp FreeRTOS/Demo/CORTEX_A9_Zynq_ZC702/RTOSDemo/src/FreeRTOSConfig.h .
+patch -p0 < patches/freertos-config.patch
+make
+
+#Creating disk image
+dtc=./build/tools/dtc/bin/dtc
+bindir=./build/disk/images/arm32/vexpress-a9
+system_dir=./build/disk/system
+img_dir=./build/disk/images/arm32
+srcdir=./tests/arm32/vexpress-a9
+cd $xvisor_src
+mkdir -p $system_dir
+mkdir -p $bindir
+cp -f ./docs/banner/roman.txt $system_dir/banner.txt
+cp -f ./docs/logo/xvisor_logo_name.ppm $system_dir/logo.ppm
+$dtc -I dts -O dtb -o $img_dir/vexpress-a9x2.dtb $srcdir/vexpress-a9x2.dts
+cp -f ./build/$srcdir/freertos/freertos.patched.bin $bindir/freertos.bin
+cp -f $srcdir/freertos/nor_flash.list $bindir/nor_flash.list
+genext2fs -B 1024 -b 16384 -d ./build/disk ./build/disk.img
