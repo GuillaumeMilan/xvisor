722488a310f8677f54e8a286d15d5cfbbcb4f482
romzie <romain.pierson@ensimag.grenoble-inp.fr>
2017-10-24 02:21:59 +0200

    Fixed background wget not waiting and make error of .h file

--- install.sh
+++ install.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 
 #Goto directory xvisor-tree
+echo "============================="
+echo "== Installing Dependencies =="
+echo "============================="
 xvisor_src=$(pwd)
 cd ..
 
@@ -9,9 +12,11 @@ sudo apt-get install bison
 sudo apt-get install genext2fs
 sudo apt-get install telnet
 
-#Install arm-eabi-gcc compiler
+echo "=================================="
+echo "== Installing arm-eabi Compiler =="
+echo "=================================="
 cd tools
-wget http://dn.odroid.in/ODROID-XU/compiler/arm-eabi-4.6.tar.gz
+wget "http://dn.odroid.in/ODROID-XU/compiler/arm-eabi-4.6.tar.gz"
 tar xvf arm-eabi-4.6.tar.gz
 
 export PATH="$PATH":$xvisor_src/../tools/arm-eabi-4.6/bin
@@ -24,21 +29,29 @@ if [ tmp -ne 0 ]; then
     exit 1
 fi
 
-#Update xvisor source
+echo "==========================="
+echo "== Update Xvisor Sources =="
+echo "==========================="
 cd $xvisor_src
 git submodule init
 git submodule update
 
-#Compile for ARM v7
-make ARCH=arm generic-v7-defconfig
-make all dtbs
+echo "=========================="
+echo "== Compiling for ARM v7 =="
+echo "=========================="
+make ARCH=arm generic-v7-defconfig -j4
+make all dtbs -j4
 
-#Setup FreeRTOS guest
+echo "=========================="
+echo "== Setup FreeRTOS Guest =="
+echo "=========================="
 cd $xvisor_src/..
-wget -O FreeRTOSv9.0.0.zip https://downloads.sourceforge.net/project/freertos/FreeRTOS/V9.0.0/FreeRTOSv9.0.0.zip?r=http%3A%2F%2Fwww.freertos.org%2Fa00104.html&ts=1508233726&use_mirror=vorboss
+wget -O FreeRTOSv9.0.0.zip "https://downloads.sourceforge.net/project/freertos/FreeRTOS/V9.0.0/FreeRTOSv9.0.0.zip?r=http%3A%2F%2Fwww.freertos.org%2Fa00104.html&ts=1508233726&use_mirror=vorboss"
 unzip FreeRTOSv9.0.0.zip
 
-#Update FreeRTOS files of xvisor
+echo "============================="
+echo "== Updating FreeRTOS files =="
+echo "============================="
 cp -r ./FreeRTOSv9.0.0/FreeRTOS $xvisor_src/tests/arm32/vexpress-a9/freertos
 cd $xvisor_src/tests/arm32/vexpress-a9/freertos
 mkdir -p port
@@ -46,9 +59,14 @@ cp FreeRTOS/Source/portable/GCC/ARM_CA9/* port/
 patch -p0 < patches/ports.patch
 cp FreeRTOS/Demo/CORTEX_A9_Zynq_ZC702/RTOSDemo/src/FreeRTOSConfig.h .
 patch -p0 < patches/freertos-config.patch
-make
+export CROSS_COMPILE=arm-eabi-
+cp ./FreeRTOS/Source/include/stdint.readme ./stdint.h
+cp ./FreeRTOS/Demo/ColdFire_MCF52259_CodeWarrior/stdlib.h ./stdlib.h
+make -j4
 
-#Creating disk image
+echo "========================="
+echo "== Creating Disk Image =="
+echo "========================="
 dtc=./build/tools/dtc/bin/dtc
 bindir=./build/disk/images/arm32/vexpress-a9
 system_dir=./build/disk/system
@@ -63,3 +81,9 @@ $dtc -I dts -O dtb -o $img_dir/vexpress-a9x2.dtb $srcdir/vexpress-a9x2.dts
 cp -f ./build/$srcdir/freertos/freertos.patched.bin $bindir/freertos.bin
 cp -f $srcdir/freertos/nor_flash.list $bindir/nor_flash.list
 genext2fs -B 1024 -b 16384 -d ./build/disk ./build/disk.img
+
+echo "==========================="
+echo "== Installation Finished =="
+echo "==========================="
+
+exit 0
